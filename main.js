ac39752a377abf03b8baec66fd3b81ef4aad86ec
Yosibalacho <yosibala@gmail.com>
2022-05-26 09:07:52 +0300

    objects

---
+++ main.js
@@ -0,0 +1,135 @@
+//1//
+// var manager = {
+//   firstname: "Yosi",
+//   lastname: "Balacho",
+//   age: 25,
+//   wage: 40000,
+// };
+
+// var employee = {
+//   fullname: "daniel gedamo",
+//   birthdate: 1995,
+//   email: "danielgedamo@gmail.com",
+//   adress: "rehovot",
+// };
+//2//
+// var fruit = {
+//   name: "banana",
+//   color: "yellow",
+//   type: "hadar",
+// };
+// document.write(fruit.name, "<br>", fruit.color, "<br>", fruit.type);
+//3//
+// var course={
+//     courseNumber: "70",
+//     studentNumber: 25,
+//     studentNames: ["yosi","yoni","daniel","nati","dalia"],
+// }
+
+// for(var i=0; i<course.studentNames.length;i++){
+//     document.write(course.studentNames[i]+"<br>")
+// }
+
+// var course = {}
+//  course.courseNumber=prompt("Enter course number");
+//  course.studentNumber=prompt("Enter number of students");
+//  course.studentAge=prompt("Enter student age")
+//  console.log(course);
+//4//
+// var teacher={}
+// var key=prompt("Enter key")
+// var value=prompt("Enter your value")
+// teacher[key]=teacherName
+// console.log(teacher);
+// var manager={}
+// for(var i=0; i<4;i++){
+// var key=prompt("Enter key")
+// var value=prompt("Enter value")
+// manager[key]=value
+// console.log(manager);
+// }
+//5//
+// var user={}
+// for(var i=0; i<4;i++){
+//    var key=prompt("Enter key")
+//    var value=prompt("Enter value")
+//    user[key]=value;
+//    document.write("<div>"+value+"</div>")
+//6//
+
+// var kindergarden = {
+//   teacherName: "batel",
+//   numberOfKids: 15,
+//   kidsNames: [
+//     "daniel",
+//     "batel",
+//     "dalia",
+//     "dani",
+//     "yosi",
+//     "yoni",
+//     "nati",
+//     "dalia",
+//     "hen",
+//     "toni",
+//   ],
+
+// };
+// for(var i=0;i<kindergarden.kidsNames.length;i++){
+
+//     document.write(kindergarden["kidsNames"][i]+"<br>");
+// }
+
+//7//
+// }
+// var carsArray=[];
+// for (var i = 0; i < 4; i++) {
+//   var car = {};
+//   car.company = prompt("enter the car brand");
+//   car.color = prompt("enter the car color");
+//   car.cc = prompt("enter the car cc");
+//   car.yearbook = +prompt("enter the  car yearbook");
+//   carsArray.push(car);
+//   document.write(
+//     "<div>" +
+//       car.company +
+//       " " +
+//       car.color +
+//       " " +
+//       car.cc +
+//       " " +
+//       car.yearbook +
+//       "</div>"
+//   );
+// }
+// console.log(carsArray);
+
+//8//
+// function cats() {
+//   for (var i = 0; i < 3; i++) {
+//     var cat = {};
+//     cat.name = prompt("enter the cat name");
+//     cat.birthdate = prompt("enter the cat birthdate");
+//     cat.type = prompt("enter the cat type");
+//     cat.waigth = prompt("enter the cat waigth");
+//     document.write(
+//         "<h1>" +
+//           cat.name +
+//           " " +
+//           cat.birthdate +
+//           " " +
+//           cat.type +
+//           " " +
+//           cat.waigth +
+//           "</h1>"
+//       );
+//   }
+// }
+// cats();
+//9//
+//  var userKey={}
+//  userKey.mykey=prompt("enter key")
+//  userKey.myValue=prompt("enter value")
+//  console.log(userKey)
+                 //10//
+function numberOfDogs() {}
+numberOfDogs();
